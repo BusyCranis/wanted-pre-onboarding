ea0fc6bcd3e1608ab01635501372c1d23469edda
cranis <cranis2@naver.com>
2022-07-10 16:28:38 +0900

    home

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,11 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
-      dark
+      
     >
-      <div class="d-flex align-center">
-     
-      </div>
+    <router-link :to="{name: 'Home'}"> 홈으로 </router-link>
+    
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
